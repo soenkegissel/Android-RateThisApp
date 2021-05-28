cfb8ffdc3e7a63dd4389d8de1496bb3656bfdb1a
Sönke Gissel <soenke.gissel@gmail.com>
2021-05-28 22:50:38 +0200

    Callback bugfix

--- ratethisapp/src/main/java/com/kobakei/ratethisapp/RateThisApp.java
+++ ratethisapp/src/main/java/com/kobakei/ratethisapp/RateThisApp.java
@@ -374,13 +374,15 @@ public class RateThisApp implements Callback {
                     // We can get the ReviewInfo object
                     mReviewInfo = task.getResult();
                     Task<Void> flow = mReviewManager.launchReviewFlow(mFragmentActivity, mReviewInfo);
+
+                    if (sCallback != null) {
+                        sCallback.onYesClicked();
+                    }
+
                     flow.addOnCompleteListener(task2 -> {
                         // The flow has finished. The API does not indicate whether the user
                         // reviewed or not, or even whether the review dialog was shown. Thus, no
                         // matter the result, we continue our app flow.
-                        if (sCallback != null) {
-                            sCallback.onYesClicked();
-                        }
                     });
                 } else {
                     // There was some problem, log or handle the error code.
